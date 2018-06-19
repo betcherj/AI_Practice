b927a9e83e226838442fccfa852b70c59d2ca848
Jack Betcher <betcherj@bc.edu>
2018-06-18 22:05:12 -0400

    'updates'

--- Minimax_tictactoe.py
+++ Minimax_tictactoe.py
@@ -1,8 +1,9 @@
-<<<<<<< Updated upstream
 #Jack Betcher
 #Minimax
 import random
 
+computerLetter = 'O'
+playerLetter = 'X'
 #draws board
 def drawBoard(board):
     print('' + board[0] + '|' + board[1] + '|' + board[2])
@@ -12,20 +13,12 @@ def drawBoard(board):
     print('' + board[6] + '|' + board[7] + '|' + board[8])
 
 
-def inputPlayerLetter():
-    letter = ''
-    while(letter == ''):
-        letter = raw_input("X's or O's\n")
-    if letter == 'X':
-        return ['X','O']
-    else:
-        return ['O','X']
-
 def whoGoesFirst():
     if random.choice([0,1])==1:
         return 'Player'
     else:
         return 'Computer'
+
 def playAgain():
     print('Do you want to play again')
     return raw_input().lower().startswith('y')
@@ -55,7 +48,7 @@ def getPlayerMove(board):
         move = str(input())
     return int(move)
 
-#Checks if moves in a list are availble and chooses one randomly 
+ 
 def checkMovesList(board, movesList):
     possibleMoves = list()
     for i in movesList:
@@ -66,37 +59,7 @@ def checkMovesList(board, movesList):
     else:
         return possibleMoves
     
-#returns the Computers Move 
-def getComputerMove(board, computerLetter):
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
 
-    #checks to see if the computer can win this turn 
-    for i in range (len(board)):
-        copy = board[:]
-        if isSpaceFree(copy, i):
-            makeMove(copy, computerLetter, i)
-            if isWinner(copy, computerLetter):
-                return i
-    #checks to see if the player can win next turn and blocks
-    for i in range (len(board)):
-        copy = board [:]
-        if isSpaceFree(copy, i):
-            makeMove(copy, playerLetter, i)
-            if isWinner(copy, playerLetter):
-                return i
-    #plays in a corner if avalible
-    move = random.choice(checkMovesList(board, [0,2,6,9]))
-    if move != None:
-        return move
-    #plays in the center if available
-    if isSpaceFree(board, 4):
-        return 4
-
-    #move on one of the sides
-    return random.choice(checkMovesList(board, [1,3,5,7]))
 #checks if the board is full 
 def isBoardFull(board):
     for i in range(len(board)):
@@ -104,182 +67,91 @@ def isBoardFull(board):
             return False
     return True 
 
-print('Unbeatable TicTacToe')
-
-
-def miniMax(board, letter):
-    moves = checkMovesList(board, [0,1,2,3,4,5,6,7,8])
-    if isWinner(board, playerLetter):
-        return -10
-    elif isWinner(board, computerLetter):
-        return 10
-    elif len(moves)==0:
+def evaluate(board):
+    if isBoardFull(board):
+        if isWinner(board, computerLetter):
+            return 10
+        elif isWinner(board, playerLetter):
+            return -10
+    else:
         return 0
-    board[moves[i]] = letter
-    if letter == computerLetter:
-        result = miniMax(board,playerLetter)
-        moves[i] = result
-    else:
-        result = miniMax(board, computerLetter)
-        moves[i] = result
-
-    moves.append[move]
-                        
-        
-    
-
-
-
-
-
-
-
-
-
-    
-=======
-#Jack Betcher
-#Minimax
-import random
-
-#draws board
-def drawBoard(board):
-    print('' + board[0] + '|' + board[1] + '|' + board[2])
-    print('-------')
-    print('' + board[3] + '|' + board[4] + '|' + board[5])
-    print('-------')
-    print('' + board[6] + '|' + board[7] + '|' + board[8])
-
-
-def inputPlayerLetter():
-    letter = ''
-    while(letter == ''):
-        letter = raw_input("X's or O's\n")
-    if letter == 'X':
-        return ['X','O']
-    else:
-        return ['O','X']
-
-def whoGoesFirst():
-    if random.choice([0,1])==1:
-        return 'Player'
-    else:
-        return 'Computer'
-def playAgain():
-    print('Do you want to play again')
-    return raw_input().lower().startswith('y')
-
-def makeMove(board, letter, move):
-    board[move] = letter
-
-#checks if board has a winner 
-def isWinner(bo, le):
-    return ((bo[0] == le and bo[1] == le and bo[2] == le) or
-            (bo[3] == le and bo[4] == le and bo[5] == le) or
-            (bo[6] == le and bo[7] == le and bo[8] == le) or
-            (bo[0] == le and bo[3] == le and bo[6] == le) or
-            (bo[1] == le and bo[4] == le and bo[7] == le) or
-            (bo[2] == le and bo[5] == le and bo[8] == le) or
-            (bo[0] == le and bo[4] == le and bo[8] == le) or
-            (bo[2] == le and bo[4] == le and bo[6] == le))
-
-def isSpaceFree(board, move):
-    return board[move] == ''
-
-#gets and returns the players move 
-def getPlayerMove(board):
-    move = '' 
-    while move not in '0 1 2 3 4 5 6 7 8'.split() or not isSpaceFree(board, int(move)):
-        print('what is your next move? (0,1,2,3,4,5,6,7,8)')
-        move = str(input())
-    return int(move)
-
-#Checks if moves in a list are availble and chooses one randomly 
-def checkMovesList(board, movesList):
-    possibleMoves = list()
-    for i in movesList:
-        if isSpaceFree(board, i):
-            possibleMoves.append(i)
-    if len(possibleMoves) == 0:
-        return None
-    else:
-        return possibleMoves
-    
-#returns the Computers Move 
-def getComputerMove(board, computerLetter):
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
-
-    #checks to see if the computer can win this turn 
-    for i in range (len(board)):
-        copy = board[:]
-        if isSpaceFree(copy, i):
-            makeMove(copy, computerLetter, i)
-            if isWinner(copy, computerLetter):
-                return i
-    #checks to see if the player can win next turn and blocks
-    for i in range (len(board)):
-        copy = board [:]
-        if isSpaceFree(copy, i):
-            makeMove(copy, playerLetter, i)
-            if isWinner(copy, playerLetter):
-                return i
-    #plays in a corner if avalible
-    move = random.choice(checkMovesList(board, [0,2,6,9]))
-    if move != None:
-        return move
-    #plays in the center if available
-    if isSpaceFree(board, 4):
-        return 4
-
-    #move on one of the sides
-    return random.choice(checkMovesList(board, [1,3,5,7]))
-#checks if the board is full 
-def isBoardFull(board):
-    for i in range(len(board)):
-        if isSpaceFree(board,i):
-            return False
-    return True 
 
 print('Unbeatable TicTacToe')
 
-
-def miniMax(board, letter):
+def getBestMove(board):
+    bestMove = None
     avail = checkMovesList(board, [0,1,2,3,4,5,6,7,8])
-    if isWinner(board, playerLetter):
-        return -10
-    elif isWinner(board, computerLetter):
-        return 10
-    elif len(avail)==0:
-        return 0
-    moves = []
+    depth = len(avail)
+    bestVal = -1000
     for i in avail:
-        move = []
-        move.append((board[i],))
-        board[i]=letter
-        if letter == computerLetter:
-            result = miniMax(board,playerLetter)
-            move[i][1] = result
+        makeMove(board, computerLetter, i)
+        val = miniMax(board, depth+1, False)
+        makeMove(board, '', i)
+        print 'val is:', val
+        if val>bestVal:
+            bestVal = val
+            bestMove = i
+            print 'best move is: ', bestMove
+    return bestMove   
+
+def miniMax(board, depth, isMax):
+    score = evaluate(board)
+    if score == 10:
+        return score
+    if score == -10:
+        return score 
+    avail = checkMovesList(board, [0,1,2,3,4,5,6,7,8])
+    if isBoardFull(board):
+        return 0
+    if isMax:
+        bestVal = -100000
+        for i in avail:
+            makeMove(board, computerLetter, i)
+            val = miniMax(board, depth+1, False)
+            bestVal = max(bestVal, val)
+            makeMove(board, '', i)
+        return bestVal
+    else:
+        bestVal = 100000
+        for i in avail:
+            makeMove(board, playerLetter, i)
+            val = miniMax(board, depth+1, True)
+            bestVal = min(bestVal, val)
+            makeMove(board, '', i)
+        return bestVal
+    
+while True:
+    board = ['']*9
+    turn = whoGoesFirst()
+    print(turn + ' goes first')
+    stillPlaying = True
+    while stillPlaying:
+        drawBoard(board)
+        if turn == 'Computer':
+            move = getBestMove(board)
+            makeMove(board, computerLetter, move)
+            turn = 'Player'
         else:
-            result = miniMax(board, computerLetter)
-            move[i][1] = result
-        board[i] = move[i][0]
-        moves.append[move]
-    bestMove=list((,))
+            move = getPlayerMove(board)
+            makeMove(board, playerLetter, move)
+            turn = 'Computer'
+        print turn
+        if isWinner(board, computerLetter):
+            print('Computer Wins')
+            drawBoard(board)
+            stillPlaying = False
+        elif isWinner(board, playerLetter):
+            print('Player Wins')
+            drawBoard(board)
+            stillPlaying = False
+        elif isBoardFull(board):
+            print('Tie')
+            drawBoard(board)
+            stillPlaying = False
+    if not playAgain():
+        break
+
+
+
+
     
-    
-        
-    
-
-
-
-
-
-
-
-
-
-    
->>>>>>> Stashed changes
